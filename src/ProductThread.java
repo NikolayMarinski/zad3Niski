ad0a95e0b7731bea2204dd7b5c47f41518079ca3
Nikolay Marinski <smgF2D1@smg.local>
2024-05-16 09:35:07 +0300

    Initial commit

---
+++ src/ProductThread.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class ProductThread  extends Thread{
+    private String fileName;
+    private PriorityBlockingQueue<Product> products;
+
+    ProductThread(String name, String fileName, PriorityBlockingQueue<Product> products){
+
+    }
+    public String getFileName(){
+        return fileName;
+    }
+    public void setFileName(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void run(){
+        File file = new File(fileName);
+        Scanner in = null;
+        try{
+            in = new Scanner(file);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        while(in.hasNext()){
+            String name = in.next();
+            String category = in.next();
+            Double price = in.nextDouble();
+            Product p = new Product(name, category, price);
+            synchronized (products){
+                products.add(p);
+            }
+        }
+
+        in.close();
+    }
+
+}
